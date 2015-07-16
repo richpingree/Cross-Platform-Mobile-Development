428cdb3b6f7922366cfdcd3bef449a28cf01fc75
richpingree <richpingree@fullsail.edu>
2015-07-15 23:26:06 -0500

    Loaded New Project from Parse docs, setup Storyboard
    Loaded New Project from Parse docs, setup Storyboard

---
+++ parse-starter-project-1.7.5/ParseStarterProject/ParseStarterProject/ParseStarterProjectAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  ParseStarterProjectAppDelegate.h
+//
+//  Copyright 2011-present Parse Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@class ParseStarterProjectViewController;
+
+@interface ParseStarterProjectAppDelegate : UIResponder <UIApplicationDelegate>
+
+//@property (nonatomic, strong) IBOutlet UIWindow *window;
+
+@property (strong, nonatomic) UIWindow *window;
+
+//@property (nonatomic, strong) IBOutlet ParseStarterProjectViewController *viewController;
+
+@end
